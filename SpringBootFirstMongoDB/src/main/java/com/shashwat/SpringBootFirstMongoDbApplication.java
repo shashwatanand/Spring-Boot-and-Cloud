d63b13fec84f953babdbdeb61f0335ce3264b47d
Shashwat Anand <shashwat.anand@gmail.com>
2015-10-25 12:23:28 +0530

    Modified the package name

--- SpringBootFirstMongoDB/src/main/java/com/shashwat/SpringBootFirstMongoDbApplication.java
+++ SpringBootFirstMongoDB/src/main/java/com/shashwat/SpringBootFirstMongoDbApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.shashwat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- SpringBootFirstMongoDB/src/test/java/com/shashwat/SpringBootFirstMongoDbApplicationTests.java
+++ SpringBootFirstMongoDB/src/test/java/com/shashwat/SpringBootFirstMongoDbApplicationTests.java
@@ -1,10 +1,12 @@
-package com.example;
+package com.shashwat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.shashwat.SpringBootFirstMongoDbApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootFirstMongoDbApplication.class)
